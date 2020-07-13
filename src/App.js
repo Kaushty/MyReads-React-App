9e331f8587c090efae7e75d6d952627e93ef760c
kaushty <kaushikshetty4898@gmail.com>
2020-07-13 16:44:29 +0530

    Initial Commit
    Created React App using create-react-app package. Added the starter
    code. Added links and route components.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+import ReadNow from "./AppComponents/ReadNow";
+import ReadNext from "./AppComponents/ReadNext";
+import ReadComplete from "./AppComponents/ReadComplete";
+import Search from "./AppComponents/SearchComponent";
+import { Link, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BooksApp extends React.Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false,
+  };
+
+  searchBook = (e) => {
+    const response = BooksAPI.search("ART").then((res) => console.log(res));
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title">Currently Reading</h2>
+                    <ReadNow />
+                  </div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title">Want to Read</h2>
+                    <ReadNext />
+                  </div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title">Read</h2>
+                    <ReadComplete />
+                  </div>
+                </div>
+              </div>
+              <div className="open-search">
+                <Link className="open-search-button" to="/search">
+                  Add a book
+                </Link>
+              </div>
+            </div>
+          )}
+        />
+        <Route exact path="/search" component={Search} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default BooksApp;
